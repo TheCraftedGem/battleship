6f5a6370951f31d3b70afc7253a19adee0deb6c5
maybemushrooms <thecraftedgem@tuta.io>
2018-09-05 19:30:11 -0600

    adds space class

---
+++ lib/space.rb
@@ -0,0 +1,30 @@
+require './lib/ship'
+
+class Space
+  attr_reader :ship,
+              :state
+
+  def initialize
+    @ship = nil
+    @state = " "
+  end
+
+  def fill(ship)
+    @ship = ship
+  end
+
+  def shot
+    if @ship == nil
+      @state = 'M'
+    else
+      @ship.hit
+      @state = 'H'
+    end
+  end
+
+  def show_sunk
+    if @ship != nil && @ship.sunk
+        @state = 'X'
+    end
+  end
+end
